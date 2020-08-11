96f900bfc8ad7476ed46044983b98412ba8f8a0f
Olatunde Sodiq <olatundesodiq@gmail.com>
2020-08-11 18:46:24 +0100

    bug fix incorrect image url

--- src/components/RecentWork.vue
+++ src/components/RecentWork.vue
@@ -15,17 +15,19 @@ export default {
     components: {
         WorkList
     },
+
     data: function () {
         return{
             imgs :[
                 {
                     id: 0,
                     name: "Bouncer App",
-                    image: "https://res.cloudinary.com/olacode/image/upload/v1585309547/personal/bouncer_xuz9ok.webp", 
+                    image: "https://res.cloudinary.com/olacode/image/upload/c_scale,w_763/v1585309547/portfolio/bouncer_xuz9ok.webp", 
                     url: "https://bouncerapp.netlify.com/",
                     description: "This is an  ecommerce application for buying and selling of conputers and phone accesories.",
 
                 },
+
                 {
                     id: 1, 
                     name: "eHealths",
@@ -37,7 +39,7 @@ export default {
                 {
                     id: 2, 
                     name: "Expense App",
-                    image: "https://res.cloudinary.com/olacode/image/upload/v1585308929/personal/expense-app_kuyeod.webp", 
+                    image: "https://res.cloudinary.com/olacode/image/upload/c_scale,w_788/v1585308929/portfolio/expense-app_kuyeod.webp", 
                     url: "https://ehealths.herokuapp.com/",
                     description: "An online expense reporting software, made for businesses to automate expense."
                 },
@@ -61,7 +63,6 @@ export default {
 .recent-work-container{
     margin: 70px 0;
     /* text-align: center; */
-
 
 }
 
